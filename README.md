1146b16df2318a41135cfa10289074ec98354218
Jason Kim <34352746+jasonminsookim@users.noreply.github.com>
2024-04-27 13:02:49 -0700

    add echo swagger middlware

---
+++ README.md
@@ -0,0 +1,11 @@
+# Components:
+
+- Neon: Postgres DB
+- HTMX: HATEOAS
+- Golang
+  - Templ: Template generatiom
+  - Jet: Sql generation
+  - Atlas: Migrations
+  - Air: Hot reloading
+  - Echo: Router
+- Tailwind CSS + DaisyUI
--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "githug.com/jasonminsookim/go-ufcstats/cmd/docs"
 )
 
 func main() {
 	e := echo.New()
-	e.Static("/", "asssets")
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
